63d25ffd5657108060b3c17823a5005084a5063d
GoranStefanovski <tevidma@gmail.com>
2025-07-29 12:31:25 +0200

    change success message

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -82,7 +82,7 @@ class ResetPasswordController extends Controller
     protected function sendResetResponse($response)
     {
         return redirect($this->redirectPath())
-            ->with('status', trans('administration::'.$response));
+            ->with('status', 'Your password has been changed successfully.');
     }
 
     /**
